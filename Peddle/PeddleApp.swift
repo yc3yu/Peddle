2d71fa6d2be8ef8bead6236fe296250534a8afe1
cindyu1020 <cindyu1020@gmail.com>
2024-03-16 00:16:46 -0700

    Initial Commit

---
+++ Peddle/PeddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeddleApp.swift
+//  Peddle
+//
+//  Created by Yue chen Yu on 2024-03-16.
+//
+
+import SwiftUI
+
+@main
+struct PeddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
