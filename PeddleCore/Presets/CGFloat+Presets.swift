de724c71267ba3bc515a90032153149f3eb044b2
cindyu1020 <cindyu1020@gmail.com>
2024-03-16 23:15:39 -0700

    Add Presets extension for preset spacings

---
+++ PeddleCore/Presets/CGFloat+Presets.swift
@@ -0,0 +1,37 @@
+//
+//  CGFloat+Presets.swift
+//  Peddle
+//
+//  Created by Yue chen Yu on 2024-03-16.
+//
+
+import Foundation
+
+extension CGFloat {
+
+    /// A value of 2.0
+    public static let nanoSpace: CGFloat = 2.0
+
+    /// A value of 4.0
+    public static let microSpace: CGFloat = 4.0
+
+    /// A value of 8.0
+    public static let miniSpace: CGFloat = 8.0
+
+    /// A value of 12.0
+    public static let littleSpace: CGFloat = 12.0
+
+    /// A value of 16.0
+    public static let smallSpace: CGFloat = 16.0
+
+    /// A value of 24.0
+    public static let mediumSpace: CGFloat = 24.0
+
+    /// A value of 32.0
+    public static let largeSpace: CGFloat = 32.0
+
+    /// A value of 64.0
+    public static let extraLargeSpace: CGFloat = 64.0
+
+}
+
