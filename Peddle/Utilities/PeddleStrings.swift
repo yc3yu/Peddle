269da74501c10c48fe94af4ba5a33365a3b0140b
cindyu1020 <cindyu1020@gmail.com>
2024-03-19 14:38:55 -0700

    Create PeddleStrings.swift to store strings, replacing AssetNames.swift

---
+++ Peddle/Utilities/PeddleStrings.swift
@@ -0,0 +1,25 @@
+//
+//  PeddleStrings.swift
+//  Peddle
+//
+//  Created by Yue chen Yu on 2024-03-19.
+//
+
+internal enum PeddleStrings {
+
+    // MARK: Strings used to reference image names
+
+    internal enum ImageNames {
+
+        // MARK: Strings used to reference image names from system images
+
+        internal enum System {
+
+            /// System image named "person.fill", for profile icon
+            internal static let profileIcon = "person.circle"
+
+        }
+
+    }
+
+}
