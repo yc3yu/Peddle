76637c8e59f8b390948df1192bd7c24b1c1daa6b
cindyu1020 <cindyu1020@gmail.com>
2024-03-19 20:31:56 -0700

    Create initial HomeView to use for TabBarView

--- Peddle/Utilities/PeddleStrings.swift
+++ Peddle/Utilities/PeddleStrings.swift
@@ -9,6 +9,9 @@ internal enum PeddleStrings {
 
     // MARK: Globally-used strings
 
+    /// "Home"
+    internal static let home = "Home"
+
     /// "Profile"
     internal static let profile = "Profile"
 
@@ -19,6 +22,9 @@ internal enum PeddleStrings {
         // MARK: Strings used to reference image names from system images
 
         internal enum System {
+
+            /// System image named "house", for home icon
+            internal static let homeIcon = "house"
 
             /// System image named "person.fill", for profile icon
             internal static let profileIcon = "person.circle"
